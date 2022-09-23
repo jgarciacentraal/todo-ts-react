41aab136a336c7290cd0c60de8b570d173e55d98
Julio Estefanes <54825763+jgarciacentraal@users.noreply.github.com>
2022-09-23 18:50:35 -0500

    some changes

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+
+import "./List.css";
+import { Input } from "../Input/Input";
 type ListProps = {
   items: {
     id: number;
@@ -7,19 +10,40 @@ type ListProps = {
   }[];
   title: string;
 };
+type State = {
+  show: boolean;
+  id: number;
+};
 export default function List(props: ListProps) {
-  console.log(props);
+  const [data, setData] = React.useState(props.items);
+  const [edit, setEdit] = React.useState<State>({
+    show: false,
+    id: 0
+  });
+  console.log(edit);
   return (
     <div className="card">
       <h1 className="heading">{props.title}</h1>
-
-      {props.items.map((t) => (
-        <li key={t.id}>
+      {data.map((t) => (
+        <li key={t.id} className="item">
           {t.task}
-          <button>edit</button>
-          <button>delete</button>
+          <div>
+            <button onClick={() => setEdit({ show: true, id: t.id })}>
+              edit
+            </button>
+            <button>delete</button>
+          </div>
         </li>
       ))}
+
+      <div className="edit-form">
+        {edit?.show ? (
+          <div className="container-edit">
+            <span>Edit</span>
+            <Input />
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
